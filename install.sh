4664fe8c9f014751f58351f42244c32c44d2a244
BosEriko <bos.eriko@gmail.com>
2024-06-27 13:27:00 +0800

    remove obsidian for notion

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@ scoop bucket add extras
 scoop bucket add main
 
 # Install Applications
-scoop install obsidian                  # Obsidian
+scoop install extras/notion             # Notion
 scoop install keepassxc                 # KeePassXC
 scoop install glazewm                   # GlazeWM
-
-# Install Extras
 scoop install extras/vscode             # Visual Studio Code
 scoop install extras/powertoys          # PowerToys
