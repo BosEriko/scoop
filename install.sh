828c49ea33505d5e88153662a1b4efdb8d2f0f7c
BosEriko <bos.eriko@gmail.com>
2024-06-29 16:31:05 +0800

    add obs

--- install.sh
+++ install.sh
@@ -4,13 +4,16 @@
 scoop bucket add extras
 scoop bucket add main
 
-# Install Applications
+# Install Work Tools
 scoop install git                       # Git (Version Control)
-scoop install extras/carnac             # Carnac (Show Key Strokes)
-scoop install extras/obsidian           # Obsidian (Notes)
-scoop install extras/keepassxc          # KeePassXC (Password Manager)
-
-## Config Dependencies
 scoop install extras/glazewm            # GlazeWM (Window Manager)
 scoop install extras/vscode             # Visual Studio Code (Editor & Terminal)
 scoop install extras/powertoys          # PowerToys (Utilities)
+
+# Install Utilities
+scoop install extras/obsidian           # Obsidian (Notes)
+scoop install extras/keepassxc          # KeePassXC (Password Manager)
+
+# Install Stream Tools
+scoop install extras/carnac             # Carnac (Show Key Strokes)
+scoop install extras/obs-studio         # OBS (Stream)
