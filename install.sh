f7dbaac9be86966d7e5cc71e5bbeb2723fd75fc5
BosEriko <bos.eriko@gmail.com>
2024-06-29 16:28:45 +0800

    add carnac and comments

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ scoop bucket add extras
 scoop bucket add main
 
 # Install Applications
-scoop install git                       # Git
-scoop install extras/obsidian           # Obsidian
-scoop install extras/keepassxc          # KeePassXC
+scoop install git                       # Git (Version Control)
+scoop install extras/carnac             # Carnac (Show Key Strokes)
+scoop install extras/obsidian           # Obsidian (Notes)
+scoop install extras/keepassxc          # KeePassXC (Password Manager)
 
 ## Config Dependencies
-scoop install extras/glazewm            # GlazeWM
-scoop install extras/vscode             # Visual Studio Code
-scoop install extras/powertoys          # PowerToys
+scoop install extras/glazewm            # GlazeWM (Window Manager)
+scoop install extras/vscode             # Visual Studio Code (Editor & Terminal)
+scoop install extras/powertoys          # PowerToys (Utilities)
