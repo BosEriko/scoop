4f5402fd330a6ed57c5f9690e829a5f2997c095a
BosEriko <bos.eriko@gmail.com>
2024-07-03 08:16:24 +0800

    add steam and epic games

--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-# Add extra bucket
+# Add dependencies
 scoop bucket add extras
 scoop bucket add main
 scoop bucket add nerd-fonts
+scoop bucket add versions
+scoop bucket add games
 
 # Install Font
 scoop install nerd-fonts/JetBrainsMono-NF-Mono
 
 # Install Work Tools
-scoop install git                       # Git
-scoop install extras/glazewm            # GlazeWM (https://github.com/BosEriko/glaze)
-scoop install extras/vscode             # Visual Studio Code (https://github.com/BosEriko/vs)
-scoop install extras/powertoys          # PowerToys (https://github.com/BosEriko/powertoys)
+scoop install git                           # Git
+scoop install extras/glazewm                # GlazeWM (https://github.com/BosEriko/glaze)
+scoop install extras/vscode                 # Visual Studio Code (https://github.com/BosEriko/vs)
+scoop install extras/powertoys              # PowerToys (https://github.com/BosEriko/powertoys)
 
 # Install Utilities
-scoop install extras/obsidian           # Obsidian (https://github.com/BosEriko/obsidian)
-scoop install extras/keepassxc          # KeePassXC (https://github.com/BosEriko/password)
-scoop install extras/vivaldi            # Vivaldi (https://github.com/BosEriko/vivaldi)
+scoop install extras/obsidian               # Obsidian (https://github.com/BosEriko/obsidian)
+scoop install extras/keepassxc              # KeePassXC (https://github.com/BosEriko/password)
+scoop install extras/vivaldi                # Vivaldi (https://github.com/BosEriko/vivaldi)
 
 # Install Stream Tools
-scoop install extras/obs-studio         # OBS (https://github.com/BosEriko/obs)
+scoop install extras/obs-studio             # OBS (https://github.com/BosEriko/obs)
+
+# Entertainment
+scoop install versions/steam                # Steam
+scoop install games/epic-games-launcher     # Epic Games
